2003ede258761b41630e0dce838c3fefc7d54335
Sai Nikith <sainikith0301@example.com>
2024-09-09 23:54:49 +0530

    started_frontend

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+
+
+
+const HomePage = () => {
+   
+  return (
+    <>
+<div>This is a NAvbar</div>
+<div className="hero-bg h-screen -mt-23px">Home</div>
+    </>
+   
+  )
+}
+
+export default HomePage
